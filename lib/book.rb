af5f9dce3e2600fd26b0569d79d62c5e620e5d3e
Matthew Steele <matthewsteeleonline@gmail.com>
2020-08-04 20:00:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -13,6 +13,27 @@ class Book
   
   def author
     @author
-d
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
 
